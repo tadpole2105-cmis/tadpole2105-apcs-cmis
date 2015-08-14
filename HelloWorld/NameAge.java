4ae0cfbec2c4aa838110c72fa212ddf5e2911a9a
cmis <cmis@cmis-hs08.(none)>
2015-08-14 14:32:10 +0700

    Print HelloWorld,ask for name and age

---
+++ HelloWorld/NameAge.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+public class NameAge
+    {
+    public static void main ( String[] args )
+        {
+         String input = JOptionPane.showInputDialog ("What is your name?");
+        
+         System.out.println("Hello,"); System.out.println(input);
+   
+        
+        
+        
+         String input = JOptionPane.showInputDialog ("What is age");   
+         System.out.println("You are,"); System.out.println(input);System.out.println("years old");   
+        }    // end mehod main
+        
+    } // end class HelloWorld
+         
