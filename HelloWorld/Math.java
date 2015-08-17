0beb17ee137fb98642964c894d161fc7171bf17f
cmis <cmis@cmis-hs12.(none)>
2015-08-17 11:53:20 +0700

    HelloWorld

---
+++ HelloWorld/Math.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+public class Math
+    {
+    public static void main ( String[] args )
+        {
+         int 1,2,sum,diff,prod,quot,modu;
+         String 1 = JOptionPane.showInputDialog("Enter any number");
+         String 2 = JOptionPane.showInputDialog("Enter another number");
+         int num1 = Integer.parseInt (1);
+         int num2 = Integer.parseInt (2);
+         
+         
+          
+        
+        }
+        {
+        
+         
+        }    // end mehod main
+        
+    } // end class HelloWorld
--- HelloWorld/NameAge.java
+++ HelloWorld/NameAge.java
@@ -3,15 +3,16 @@ public class NameAge
     {
     public static void main ( String[] args )
         {
-         String input = JOptionPane.showInputDialog ("What is your name?");
+         String name = JOptionPane.showInputDialog ("What is your name?");
         
-         System.out.println("Hello,"); System.out.println(input);
-   
+         System.out.println("Hello,"); System.out.println(name);
+         String age = JOptionPane.showInputDialog ("What is age");   
+         System.out.println("You are,"); System.out.println(age);System.out.println("years old");  
         
+        }
+        {
         
-        
-         String input = JOptionPane.showInputDialog ("What is age");   
-         System.out.println("You are,"); System.out.println(input);System.out.println("years old");   
+         
         }    // end mehod main
         
     } // end class HelloWorld
