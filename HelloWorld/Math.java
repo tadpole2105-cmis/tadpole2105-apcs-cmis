e7954aaaa27dc3f9c74bca1735fd5edd8736f6ec
cmis <cmis@cmis-hs07.(none)>
2015-08-18 09:57:54 +0700

    HelloWorldAddInput

--- HelloWorld/Math.java
+++ HelloWorld/Math.java
@@ -3,14 +3,37 @@ public class Math
     {
     public static void main ( String[] args )
         {
-         int 1,2,sum,diff,prod,quot,modu;
-         String 1 = JOptionPane.showInputDialog("Enter any number");
-         String 2 = JOptionPane.showInputDialog("Enter another number");
-         int num1 = Integer.parseInt (1);
-         int num2 = Integer.parseInt (2);
+         float sum= 0;
+         String One = JOptionPane.showInputDialog("Enter any number");
+         String Two = JOptionPane.showInputDialog("Enter another number");
+         float num1 = Float.parseFloat (One);
+         float num2 = Float.parseFloat (Two);
          
+         sum= num2 + num1;
+         System.out.println( +num1 + "+" +num2 + "=" +sum);
+         System.out.println( +num2 + "+" +num1 + "=" +sum);
          
-          
+         float dif= 0;
+         dif=  num2 - num1;
+         System.out.println(+num1 + "-" +num2 + "=" +dif);
+         System.out.println(+num2 + "-" +num1 + "=" +dif);
+         
+         float mul=0;
+         mul= num2*num1;
+         System.out.println(+num1 + "x" +num2 + "=" +mul);
+         System.out.println(+num2 + "x" +num1 + "=" +mul);
+         
+         float quo1=0;
+         float quo2=0;
+         quo1= num2/num1;
+         quo2= num1/num2;
+         System.out.println(+num1 + "/" +num2 + "=" +quo2);
+         System.out.println(+num2 + "/" +num1 + "=" +quo1);
+        
+         float mod1=0;
+         float mod2=0;
+         mod1= num1%num2;
+         mod2= num2%num1;
         
         }
         {
@@ -18,4 +41,4 @@ public class Math
          
         }    // end mehod main
         
-    } // end class HelloWorld
+    } // end class Math
