e8873772a215a9fab9d781eb6d699d963fef46cd
cmis <cmis@cmis-hs08.(none)>
2015-09-03 13:46:24 +0700

    Fun9

--- Fundamentals9/Fundamentals9.java
+++ Fundamentals9/Fundamentals9.java
@@ -3,19 +3,22 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 public class Fundamentals9
 {
+   public static void main ( String[] args )
+   {
    String username = JOptionPane.showInputDialog ("Username :");
    String a = JOptionPane.showInputDialog ("Password :");
    
-   int password = Integer.parseInt (a);
+   int password = Integer.parseInt(a);
    
-   if ( && )
+   if (username.equals("Tadpole") && password == 2105)
    {
+       System.out.print("You are logged in");
        
    }// end if 
-   
    else
    {
+       System.out.print("Incorrect username or password");
        
    }// end else 
-  
+   }//end main
 }// end fun9
