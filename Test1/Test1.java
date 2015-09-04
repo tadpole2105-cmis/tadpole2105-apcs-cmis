1fff0db1830610e8c8ca008d75ef964927c7a13f
cmis <cmis@cmis-hs08.(none)>
2015-09-04 11:56:51 +0700

    test

--- Test1/Test1.java
+++ Test1/Test1.java
@@ -22,10 +22,58 @@ public class Test1
             {
                 for (int num2=0; num2<array.length; num2++)
                 {
+                    
+                    int e=0;
+                    int o=0;
                     array[num2] = (double)(Math.random()*(50+1));
-                    String reverse = //change array num to string 
+                    int newarray[] = (int)(array[num2]); //change every number from array to int
+                    if (newarray[num2]%2=0)//seperate if odd or even
+                    
+                    {
+                          
+                    }//end if 
+                    
+                    else 
+                    
+                    {
+                        
+                    }// end else
+                    String[] reverse = new String[arrray[num]];//change array num to string to isolate each
+                    int oddcounter= 0;
+                    int evencounter= 0;
+                    for (num3=0; num3<reverse[num].length; num3++)// for every letter in reverse
+                    {
+                        
+                        int individual = Integer.parseInt(reverse[num3]);//change string letter back to int 
+                        if (reverse[num3]%2=0)
+                         {
+                             evencounter++;
+                             
+                         }//end if 
+                        
+                        else
+                         {
+                             oddcounter++;
+                         }//end else 
+                        
+                         // make counter equal to how many Es and Os
+                    for (num5=0; num5<evencounter.length; num5++)
+                    {
+                    }//end for 
+                    for (num5=0; num5<evencounter.length; num5++)
+                    
+                    {
+                    }//end for 
+                        
+                    }// end counter odd even
+                    
+                    evencounternew = evencounter;
+                    oddcounternew = oddcounter;
+                    
+                    
+                    System.out.println(num1 + "\t" + reverse);
                 }//reverse random
-                System.out.println(num1 + "\t" + reverse);
+                
             }// end for loop
         
     }//end main
