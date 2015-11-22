21b5297e03eddaa50eb3db35ad9711dd620c5ff4
tadpole2105-cmis <tadpole2105@cmis.ac.th>
2015-11-22 09:40:31 +0700

    m3

--- ClassMethod3/Student.java
+++ ClassMethod3/Student.java
@@ -7,6 +7,9 @@ public class Student
     private String lname;
     private int age;
     
+    private double[] studentGradesEmpty= new double[5];
+    private double[] studentGradesSubjects= {english, math, science, arts, social};
+    
     
     private double A=4.00;
     private  double Am=3.70;
@@ -16,31 +19,89 @@ public class Student
 
     
     
+    //these are the random grades (0.0 -4.0) for each subject that will be put into the setter method 
+    private double english = Math.random()*4;
+    private double math = Math.random()*4;
+    private double science = Math.random()*4;
+    private double arts = Math.random()*4;
+    private double social = Math.random()*4;
     
-    
-    public void grades (double A, double Am, double Bp, double B, double Bm)
+    /////////////////////////// might not use
+    public Student(double english, double math, double science, double arts, double social)
      {
-        this.A=A;
-        this.Am=Am;
-        this.Bp=Bp;
-        this.B=B;
-        this.Bm=Bm;
+        this.english=english;
+        this.math=math;
+        this.science=science;
+        this.arts=arts;
+        this.social=social;
      }
+    //////////////////////////// might not use 
     
-
     
+    
+    public double getGrades()
+    {
+        double[] studentGrades ={this.A, this.Am, this.Bp, this.B, this.Bm };
+        return studentGrades;
+    } //put values of grades A, Am.... into each slot of studentGrades array
+     
     public Student()
     {
        fname="Lek";
        lname="Wang";
        age=6;
-       grades=0.0;
-    }//end userinout constructor 
+       for (int i=0; i<studentGrades.length; i++)
+       {
+           studentGrades[i]=0.0;
+       }//set all slots of studentGrades array to 0.0
+    }//end single arg constructor 
+    
+    
+    
+    public Student(String fname, String lname, int age, double grades)///put array in muti agr?
+    {
+        this.fname=fname;
+        this.lname=lname;
+        this.age=age;
+        for (int i=0; i<studentGrades.length; i++)
+       {
+           this.grades=grades;
+           studentGrades[i]=grades; // put grades from multi arg into each slots of studentGrades array 
+       }//set all slots of studentGrades array to 0.0
+        this.grades=grades;
+    }//end multi arg 
+    
+    
+    public void setgrades(double newgrade)
+    {
+         for (int i=0; i<studentGrades.length; i++)
+         {
+             if (studentGrades[i]<=4.0 && studentGrades[i]>3.70)
+             {
+                 studentGrades[i]= "A";            
+             }
+             else if (studentGrades[i]<=3.70 && studentGrades[i]>3.30)
+             {
+                 studentGrades[i]= "Am";            
+             }
+             else if (studentGrades[i]<=3.30 && studentGrades[i]>3.00)
+             {
+                 studentGrades[i]= "Bp";            
+             }
+             else if (studentGrades[i]<=3.0 && studentGrades[i]>2.70)
+             {
+                 studentGrades[i]= "B";            
+             }
+             else
+                 studentGrades[i]= "Bm";
+         }
+    }
+    
     
     public String toString()
     {
         String output = new String();
-        output = "Name: " + lname + ", " +fname +"\n" + "Age : "+ age + "\n" + "Is Thai? : " + isThai ;
+        output = "Name: " + lname + ", " +fname +"\n" + "Age : "+ age + "\n" + "Grade : " + grades ;
         return output;
     }
 }//end class MySOng
--- ClassMethod3/StudentApp.java
+++ ClassMethod3/StudentApp.java
@@ -5,17 +5,14 @@ public class StudentApp
     public static void main(String[] args)
     {
 
-        int randomgrade= Math.random()*4;
+
        
-        grades[] studentgrades = new grades[5];
-       for (int 1=0; i<grades.length; i++)
-       {
-        if ()
-        }
+
+   
        
        
        Student Senior001=new Student(); 
-       Student Junior001= new Student("Ron","Weasley",16, 0.0,0.0,0.0,0.0,0.0,);
+       Student Junior001= new Student("Ron","Weasley",16, {0.0,0.0,0.0,0.0,0.0});
       
         JOptionPane.showMessageDialog(null,Senior001,  "senior:",JOptionPane.PLAIN_MESSAGE );
         JOptionPane.showMessageDialog(null,Junior001,  "Junior:",JOptionPane.PLAIN_MESSAGE );
