5fde2b349377bd05ea2583b93ba5d0dab4db9795
cmis <cmis@cmis-hs01.(none)>
2016-01-14 14:33:09 +0700

    p1

---
+++ ClassCompP1/Ipods.java
@@ -0,0 +1,17 @@
+import Java.Util.ArrayList;
+public class Ipods
+{
+        private String color;
+        private int memory;
+        private Songs[] songLibrary;
+        
+        public ipod()
+        {
+            this.color="Silver";
+            this.memory= 16;
+            this.songLibrary= new String[3];
+        }
+        
+    
+
+}
